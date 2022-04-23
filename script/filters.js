52b9064aa458099c0fab0447b4d4a304449ea44e
rijuabraham07 <rijuabraham07@gmail.com>
2022-04-23 16:33:28 -0500

    updated pathname

--- script/filters.js
+++ script/filters.js
@@ -1,9 +1,9 @@
 
-const req = new XMLHttpRequest()
+var req = new XMLHttpRequest()
 
 req.open ('GET', chrome.extension.getURL('icons/filters.svg'))
 res.addEventListener('load',function(e){
-    const filter = req.responseXML.documentElement
+    var filter = req.responseXML.documentElement
     filter.style.display = 'none'
     filter.width = 0
     filter.height = 0
